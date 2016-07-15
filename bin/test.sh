04b14ff7fe3e5c7138e4dbc6a32aa1f0d4937947
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-07-15 13:07:32 +0900

    Support string

--- bin/test.sh
+++ bin/test.sh
@@ -49,8 +49,9 @@ testast '(+ (+ 1 (* 2 3)) 4)' '1+2*3+4;'
 testast '(+ (* 1 2) (* 3 4))' '1*2+3*4;'
 testast '(+ (/ 4 2) (/ 6 3))' '4/2+6/3;'
 testast '(/ (/ 24 2) 4)' '24/2/4;'
-
 testast '(= a 3)' 'a=3;'
+
+testast '"abc"' '"abc";'
 
 testast 'a()' 'a();'
 testast 'a(b,c,d,e,f,g)' 'a(b,c,d,e,f,g);'
@@ -71,6 +72,7 @@ test 102 'a=1;b=48+2;c=a+b;c*2;'
 
 test 25 'sum2(20, 5);'
 test 15 'sum5(1, 2, 3, 4, 5);'
+test a3 'printf("a");3;'
 
 testfail '0abc;'
 testfail '1+;'
